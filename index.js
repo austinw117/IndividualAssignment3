586e0e0ba5743c793f3561343a2414a2aa540fda
austin <123434347+austinw117@users.noreply.github.com>
2024-03-22 23:08:20 -0600

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var app = require( 'express' )();
-var http = require( 'http' ).createServer( app ); 
-const PORT = 3000; 
-app.get( '/', function( req, res ) { res.send( 'Hello World' ); }); 
-http.listen( PORT, function() { console.log( 'listening on *:' + PORT ); });
-app.get( '/', function( req, res ) { res.sendFile( __dirname + '/public/index2.html' ); });
